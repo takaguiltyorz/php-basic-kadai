8cb3df2c421ef2ae39084c5f0977adce2bfbd8dc
takaguiltyorz <40228335+takaguiltyorz@users.noreply.github.com>
2024-12-31 15:45:02 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,26 @@
+<?php
+$score1 = 80;
+$score2 = 60;
+$score3 = 55;
+$score4 = 40;
+$score5 = 100;
+$score6 = 80;
+$score7 = 95;
+$score8 = 30;
+$score9 = 60;
+$score10 = 25;
+
+echo $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+echo '<br>';
+
+echo ($score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10)/10;
+
+
+
+?>
+
+
+
+
+
